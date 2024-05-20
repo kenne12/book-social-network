53c052ceeaacff4df6328fdd054b3751b4e9d16c
Kenne Gervais <kennegervais@gmail.com>
2024-05-20 14:27:07 +0200

    Fix origin bugs

--- book-network-api/src/main/java/com/alibou/book/config/BeansConfig.java
+++ book-network-api/src/main/java/com/alibou/book/config/BeansConfig.java
@@ -5,11 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
@@ -23,24 +18,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BeansConfig {
 
-    //private final UserDetailsService userDetailsService;
-
     //@Value("${application.cors.origins:*}")
     @Value("${application.cors.origins}")
     private List<String> allowOrigins;
-
-    /* @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
-    }*/
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -52,40 +32,13 @@ public class BeansConfig {
         return new ApplicationAuditAware();
     }
 
-//    @Bean
-//    public CorsFilter corseFilter() {
-//        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//
-//        final CorsConfiguration config = new CorsConfiguration();
-//
-//        config.setAllowCredentials(true);
-//        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-//        config.setAllowedHeaders(List.of(
-//                        HttpHeaders.ORIGIN,
-//                        HttpHeaders.CONTENT_TYPE,
-//                        HttpHeaders.ACCEPT,
-//                        HttpHeaders.AUTHORIZATION
-//                )
-//        );
-//
-//        config.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-//                "Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
-//                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
-//
-//        config.setAllowedMethods(List.of("GET", "POST", "DELETE", "PUT", "PATCH"));
-//        config.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
-//                "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "File-Name"));
-//
-//        source.registerCorsConfiguration("/**", config);
-//        return new CorsFilter(source);
-//    }
-
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        //corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(allowOrigins);
+        corsConfiguration.setAllowCredentials(true);
+        //corsConfiguration.setAllowedOrigins(allowOrigins);
+        corsConfiguration.setAllowedOrigins(List.of("*"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
