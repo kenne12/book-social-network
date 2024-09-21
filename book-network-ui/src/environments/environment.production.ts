c1cabc2f67dafcd80a4afc03b0c2970700091acf
Gervais Kenne <kennegervais@gmail.com>
2024-09-21 14:20:27 +0200

    Update Deployment config

--- book-network-ui/src/environments/environment.production.ts
+++ book-network-ui/src/environments/environment.production.ts
@@ -1,5 +1,7 @@
 export const environment = {
-    API_URL: "http://192.168.1.145:8088/api/v1",
-    KEYCLOAK_SERVER_URL: "http://192.168.1.145:9090",
+    //API_URL: "http://192.168.1.145:8088/api/v1",
+    //KEYCLOAK_SERVER_URL: "http://192.168.1.145:9090",
+    API_URL: "http://192.168.1.10:8088/api/v1",
+    KEYCLOAK_SERVER_URL: "http://192.168.1.10:9090",
     KEYCLOAK_CLIENT_ID: "bsn"
 };
