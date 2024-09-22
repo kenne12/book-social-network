8c4f863761936bf533ab9c45bd3041aa42c3e3a1
Gervais Kenne <kennegervais@gmail.com>
2024-09-22 15:21:32 +0200

    Test pipe with brama

--- book-network-api/src/main/java/com/alibou/book/BookNetworkApiApplication.java
+++ book-network-api/src/main/java/com/alibou/book/BookNetworkApiApplication.java
@@ -1,7 +1,9 @@
 package com.alibou.book;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -12,5 +14,12 @@ public class BookNetworkApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookNetworkApiApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init() {
+        return (args) -> {
+            System.out.println("Initializing...");
+        };
     }
 }
